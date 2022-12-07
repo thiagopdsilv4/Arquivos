370999e0642661418024ec776e3c69a4d151f49d
Thiago Pinheiro <thiagopdsilva1@gmail.com>
2022-12-07 10:42:02 -0300

    BufferedWriter-FileWriter
    adiciona ao arquivo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,37 +1,27 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) throws IOException {
 
+
+		String[]lines = new String[] {"Good morning ", "Good afternoon", "Good night"};
+		
 		String path = "C:\\Users\\Thiag\\Documents\\javaa\\in.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-
-			String line = br.readLine();
-
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();// esquema básico de ler arquivo com bufferedreader
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){//adicionar o parametro true, indica que não quer recriar o arquivo mas sim adicionar o conteudo
+			for(String line: lines) {
+				bw.write(line);
+				bw.newLine();//para dar quebra de linha
 			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		}
+		catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
 
-/*
- * 
- * File file = new File("C:\\Users\\Thiag\\Documents\\javaa\\in.txt"); Scanner
- * sc = null;//apenas para ter algum valor
- * 
- * try { sc = new Scanner(file); while(sc.hasNextLine()) {
- * System.out.println(sc.nextLine()); } } catch(IOException e){
- * System.out.println("Error: " + e.getMessage()); } finally { if(sc != null) {
- * sc.close(); }
- */
